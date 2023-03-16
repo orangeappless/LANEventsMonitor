aa0a91cf6062ea872387b698f28b193b6c49479f
Wilfred Ta <wilf.ta0072@gmail.com>
2023-03-15 21:59:49 -0700

    fixed: DELETE_USER events now displaying proper users

--- modules/user_watcher.py
+++ modules/user_watcher.py
@@ -14,26 +14,32 @@ def start_user_watcher(audit_log, socket):
 
             for audit_type in audit_types:
                 if audit_type in new_data:
-                    data_attr = new_data.split(' ')
-                    data_attr = dict((s.split('=')+[1])[:2] for s in data_attr)
-                    # print(data_attr)
-
                     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    new_data = new_data.split('type=')
 
-                    if data_attr['type'] == 'ADD_USER':
+                    for data in new_data:
+                        if data.startswith('ADD_GROUP'):
+                            data_attr = new_data[2].split(' ')
+                            data_attr = dict((s.split('=')+[1])[:2] for s in data_attr)
+                            # print(data_attr)
+        
                             data_attr['AUID'] = data_attr['AUID'].strip('\n')
-                            notification = f"[{current_time}] {data_attr['type']} {data_attr['acct']} by {data_attr['AUID']}"
+                            notification = f"[{current_time}] USER ADDED: {data_attr['acct']} by {data_attr['AUID']}"
 
                             print(notification)
                             socket.send(notification.encode('utf-8'))
-                    elif data_attr['type'] == 'DEL_USER':
-                        try:
-                            data_attr['ID'] = data_attr['ID'].strip('\n')
-                            notification = f"[{current_time}] {data_attr['type']} {data_attr['ID']} by {data_attr['AUID']}"
+                        elif data.startswith('DEL_USER'):
+                            try:
+                                data_attr = new_data[2].split(' ')
+                                data_attr = dict((s.split('=')+[1])[:2] for s in data_attr)
+                                # print(data_attr)
 
-                            print(notification)
-                            socket.send(notification.encode('utf-8'))
-                        except:
-                            print("User does not exist")
+                                data_attr['AUID'] = data_attr['AUID'].strip('\n')
+                                notification = f"[{current_time}] USER DELETED: {data_attr['acct']} by {data_attr['AUID']}"
 
-            # time.sleep(1)
+                                print(notification)
+                                socket.send(notification.encode('utf-8'))
+                            except:
+                                print("Tried to remove a user which does not exist")
+
+            time.sleep(1)
