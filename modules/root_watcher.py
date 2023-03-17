41ce5bb2a42b14b5a0fe2f90d63cbabd90d66b30
Wilfred Ta <wilf.ta0072@gmail.com>
2023-03-17 16:59:20 -0700

    root_watcher now checks for more root login commands

--- modules/root_watcher.py
+++ modules/root_watcher.py
@@ -15,7 +15,8 @@ def start_root_watcher(audit_log, socket):
             if new_data:
                 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-                # Checks for `sudo su` attempts
+                # Parse for root login commands in log entry
+                # Currently tracks `sudo su`, `sudo -s`, `sudo su -`, `sudo -i`
                 if 'USER_CMD' in new_data:
                     new_data = new_data.replace("'", " ")
                     new_data = new_data.replace('"', "")
@@ -25,18 +26,17 @@ def start_root_watcher(audit_log, socket):
                     data_attr['AUID'] = data_attr['AUID'].strip('\n')
                     data_attr['UID'] = data_attr.pop('\x1dUID')
                     # print(data_attr) 
-                    
-                    # Parse for `sudo su` command in log entry
-                    if data_attr['exe'] == "/usr/bin/sudo" and data_attr['cmd'] == "su":
+
+                    if data_attr['exe'] == "/usr/bin/sudo" and (data_attr['cmd'] == "su" or data_attr['cmd'] == "/bin/bash" or data_attr['cmd'] == '7375202D' or data_attr['cmd'] == '-bash'):
                         if data_attr['res'] == 'success':
-                            notification = f"[{current_time}] command `sudo su` SUCCESS by \"{data_attr['UID']}\""
+                            notification = f"[{current_time}] root login SUCCESS by \"{data_attr['UID']}\""
                             
-                            # print(notification)
-                            socket.send(notification.encode('utf-8'))
+                            print(notification)
+                            socket.send(notification.encode('utf-8'))                            
                         elif data_attr['res'] == 'failed':
-                            notification = f"[{current_time}] command `sudo su` FAILED by \"{data_attr['UID']}\""
+                            notification = f"[{current_time}] root login FAILED by \"{data_attr['UID']}\""
                             
-                            # print(notification)
+                            print(notification)
                             socket.send(notification.encode('utf-8'))
 
             # time.sleep(1)
