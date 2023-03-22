5ea550e15167d6cd389ea0cb5b111ab86fc72524
Wilfred Ta <wilf.ta0072@gmail.com>
2023-03-21 20:50:55 -0700

    init thread lvl management, ssh_watcher now reacts to threat lvl

--- client.py
+++ client.py
@@ -20,6 +20,13 @@ def parse_config(config_file_path):
     return config_parser
 
 
+def init_threat_file(threat_file):
+    with open(f'utilities/{threat_file}', 'r+') as file:
+        file.seek(0)
+        file.write(str(0) + '\n')
+        file.truncate()
+
+
 def create_secure_socket(socket):
     certfile = 'certs/domain.crt'
     keyfile = 'certs/domain.key'
@@ -33,6 +40,16 @@ def main():
     # Parse config file
     config_file = "config.ini"
     configs = parse_config(config_file)
+
+    # Reinitialize threat file on each startup; sets current threat of client to 0
+    threat_file = dict(configs.items('THREAT_MGMT'))['threat_file']     # File which stores client's current threat level
+    init_threat_file(threat_file)
+
+    # Set threat levels
+    threat_levels_config = dict(configs.items('THREAT_LEVELS'))
+    max_threat = int(threat_levels_config['max_threat'])
+    mid_threat = int(threat_levels_config['mid_threat'])
+    default_threat = int(threat_levels_config['default_threat'])
 
     # Connect to server
     socket_ = socket.socket()
@@ -80,10 +97,11 @@ def main():
     thread_list.append(root_watcher_thread)
     
     # Monitor incoming SSH logins
-    root_log_file = dict(configs.items('SSH_WATCHER'))['log']
+    ssh_watcher_configs = dict(configs.items('SSH_WATCHER'))
+    ssh_audit_log = ssh_watcher_configs['log']
     ssh_watcher_thread = Thread(
         target=ssh_watcher.start_ssh_watcher,
-        args=(root_log_file, secure_socket, dict(configs.items('SSH_WATCHER'))['max_attempts'], dict(configs.items('SSH_WATCHER'))['block_time'])
+        args=(ssh_audit_log, secure_socket, ssh_watcher_configs['block_time'], threat_file, max_threat, mid_threat, default_threat)
     )
     thread_list.append(ssh_watcher_thread)
 
---
+++ utilities/threat_mgmt.py
@@ -0,0 +1,32 @@
+import configparser
+
+
+def get_threat_level(threat_file):
+    with open(f'utilities/{threat_file}') as file:
+        current_threat = int(file.read())
+
+    return current_threat
+
+
+def update_threat(action, threat_file, iters=1):
+    # Get threat levels from config file
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    config_items = dict(config.items('THREAT_MGMT'))
+
+    threat_levels = {
+        'dir_modification': int(config_items['dir_modification']),
+        'failed_ssh': int(config_items['failed_ssh']),
+        'success_ssh': int(config_items['success_ssh']),
+        'clear_ssh': int(config_items['clear_ssh'])
+    }
+
+    with open(f'utilities/{threat_file}', 'r+') as file:
+        # Read current threat level
+        current_threat = int(file.read())
+        
+        # Write updated threat level
+        file.seek(0)
+        current_threat += (threat_levels[action] * iters)
+        file.write(str(current_threat) + '\n')
+        file.truncate()
