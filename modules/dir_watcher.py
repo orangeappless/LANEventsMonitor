699db6920d606d20a1f22839d7d2df95d67f8a80
Wilfred Ta <wilf.ta0072@gmail.com>
2023-03-26 23:02:15 -0700

    ignores some swap files in dir_watcher

--- modules/dir_watcher.py
+++ modules/dir_watcher.py
@@ -5,6 +5,12 @@ from datetime import datetime
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
+        if "swp" in event.name or "swpx" in event.name or ".part" in event.name or event.name[-1] == "+" or event.name[-1] == "-" or ".lock" in event.name or "." in event.name or '~' in event.name:
+            return
+        
+        if (event.name).isdigit():
+            return
+        
         # Creation in directory
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  
         notification = f"[{current_time}] CREATED: {event.pathname}"
@@ -13,6 +19,12 @@ class EventHandler(pyinotify.ProcessEvent):
         self.send_notif(notification)
 
     def process_IN_DELETE(self, event):
+        if "swp" in event.name or "swpx" in event.name or ".part" in event.name or event.name[-1] == "+" or event.name[-1] == "-" or ".lock" in event.name or "." in event.name or '~' in event.name:
+            return
+
+        if (event.name).isdigit():
+            return
+
         # Deletion in directory
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  
         notification = f"[{current_time}] DELETED: {event.pathname}"
@@ -21,6 +33,12 @@ class EventHandler(pyinotify.ProcessEvent):
         self.send_notif(notification)
 
     def process_IN_CLOSE_WRITE(self, event):
+        if "swp" in event.name or "swpx" in event.name or ".part" in event.name or event.name[-1] == "+" or event.name[-1] == "-" or ".lock" in event.name or "." in event.name or '~' in event.name:
+            return
+
+        if (event.name).isdigit():
+            return
+
         # Change in file in directory
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  
         notification = f"[{current_time}] MODIFIED: {event.pathname}"
@@ -38,7 +56,7 @@ def start_watcher(directories, socket):
     socket_ = socket
 
     watch_manager = pyinotify.WatchManager()
-    mask = pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_CLOSE_WRITE
+    mask = pyinotify.IN_DELETE | pyinotify.IN_CLOSE_WRITE
     
     notifier = pyinotify.ThreadedNotifier(watch_manager, EventHandler())
 
