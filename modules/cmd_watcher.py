ad529b049f23b48686811305e8b9365bba9f6086
Wilfred Ta <wilf.ta0072@gmail.com>
2023-03-27 18:40:12 -0700

    automatically block blocked_cmds with setfacl

--- modules/cmd_watcher.py
+++ modules/cmd_watcher.py
@@ -27,8 +27,32 @@ def init_auditd_rules(cmds_to_add, type):
         print(f'audid watches for blocked cmds \"{cmds_to_add}\" created')
     else:
         print(f'audid watches for watched cmds \"{cmds_to_add}\" created')
+
+
+def block_cmds(blocked_cmds):
+    # setfacl -m o:--- /usr/bin/telnet
+    # setfacl -m o:rx /usr/bin/telnet
+    cmds = blocked_cmds.split(',')
+    binaries = []
+
+    # Find location of binaries first
+    for cmd in cmds:
+        which_cmd = ['which', cmd]
+        exec_which_cmd = subprocess.run(which_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = exec_which_cmd.stdout.decode('utf-8').strip('\n')
+        binaries.append(output)
+
+    for binary in binaries:
+        cmd = ['setfacl', '-m', 'o::---', f'{binary}']
+        exec_cmd = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
+    print(binaries)
+    print('cmds blocked')
+
 
 def start_cmd_watcher(audit_log, socket, threat_file, blocked_cmds, watched_cmds, threat_max, threat_mid, threat_default, block_time):
     init_auditd_rules(blocked_cmds, 'blocked-cmd')
     init_auditd_rules(watched_cmds, 'watched-cmd')
+
+    # Use setfacl to block blocked_cmds right away
+    block_cmds(blocked_cmds)
