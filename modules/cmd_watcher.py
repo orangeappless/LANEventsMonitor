b811852d554a6ca542d7ac809d54e95499c9ce24
Wilfred Ta <wilf.ta0072@gmail.com>
2023-03-28 00:06:24 -0700

    cmd_watcher interacts with threat level

--- modules/cmd_watcher.py
+++ modules/cmd_watcher.py
@@ -29,7 +29,7 @@ def init_auditd_rules(cmds_to_add, type):
         print(f'audid watches for watched cmds \"{cmds_to_add}\" created')
 
 
-def block_cmds(blocked_cmds):
+def block_blocked_cmds(blocked_cmds):
     # setfacl -m o:--- /usr/bin/telnet
     # setfacl -m o:rx /usr/bin/telnet
     cmds = blocked_cmds.split(',')
@@ -46,8 +46,41 @@ def block_cmds(blocked_cmds):
         cmd = ['setfacl', '-m', 'o::---', f'{binary}']
         exec_cmd = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
-    print(binaries)
-    print('cmds blocked')
+    # print(binaries)
+    print(f'facl rule automatically set for blocked cmds \"{blocked_cmds}\"')
+
+
+def block_watched_cmds(executed_cmds, user, block_time, threat_file, socket):
+    # setfacl -m u:username:--- /path/to/binary
+    # setfacl -x u:username /path/to/binary
+    for cmd in executed_cmds:
+        # Use setfacl to block user access to these commands
+        block_cmd = ['setfacl', '-m', f'u:{user}:---', f'{cmd}']
+        exec_block_cmd = subprocess.run(block_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    time_of_block = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    block_notification = f"[{time_of_block}] possible INCIDENT, blocking watchlisted command(s) for {user}"
+    
+    print(block_notification)
+    socket.sendall(block_notification.encode('utf-8'))
+
+    # Unblock after block timing has expired
+    unblock_cmds_timer = Timer(int(block_time), unblock_watched_cmds, args=(executed_cmds, user, threat_file, socket))
+    unblock_cmds_timer.start()
+
+
+def unblock_watched_cmds(executed_cmds, user, threat_file, socket):
+    time_of_unblock = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    unblock_notification = f"[{time_of_unblock}] unblocking watchlisted command(s) for {user}"
+
+    for cmd in executed_cmds:
+        unblock_cmd = ['setfacl', '-x', f'u:{user}', f'{cmd}']
+        exec_unblock_cmd = subprocess.run(unblock_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    threat_mgmt.update_threat('clear_exec_watched_cmd', threat_file, len(executed_cmds))
+
+    print(unblock_notification)
+    socket.sendall(unblock_notification.encode('utf-8'))
 
 
 def start_cmd_watcher(audit_log, socket, threat_file, blocked_cmds, watched_cmds, threat_max, threat_mid, threat_default, block_time):
@@ -55,4 +88,67 @@ def start_cmd_watcher(audit_log, socket, threat_file, blocked_cmds, watched_cmds
     init_auditd_rules(watched_cmds, 'watched-cmd')
 
     # Use setfacl to block blocked_cmds right away
-    block_cmds(blocked_cmds)
+    block_blocked_cmds(blocked_cmds)
+
+    executed_watched_cmds = []
+
+    file_size = os.stat(audit_log).st_size
+
+    with open(audit_log, 'r') as log_file:
+        log_file.seek(file_size)
+
+        while True:
+            new_data = log_file.read()
+
+            if new_data:
+                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                if 'watched-cmd' in new_data:
+                    new_data = new_data.split('type=')
+
+                    # We want only the SYSCALL and EXECVE entries
+                    syscall_entry = ''
+                    execve_entry = ''
+
+                    for data in new_data:
+                        if data.startswith('SYSCALL'):
+                            syscall_entry = data
+                        
+                        if data.startswith('EXECVE'):
+                            execve_entry = data
+                    
+                    # Process syscall_entry and execve_entry
+                    syscall_entry = syscall_entry.replace('"', "")
+                    syscall_entry = syscall_entry.split(' ')
+                    syscall_attrs = dict((s.split('=')+[1])[:2] for s in syscall_entry)
+                    # print(syscall_attrs)
+
+                    execve_entry = execve_entry.replace('"', "")
+                    execve_entry = execve_entry.split(' ')
+                    execve_attrs = dict((s.split('=')+[1])[:2] for s in execve_entry)
+                    # print(execve_attrs)
+
+                    # Handle threat level
+                    if syscall_attrs['exe'] != '/usr/bin/bash':
+                        threat_mgmt.update_threat('exec_watched_cmd', threat_file)
+                        executed_watched_cmds.append(syscall_attrs['exe'])
+
+                        current_threat_level = threat_mgmt.get_current_level(threat_file)
+
+                        if current_threat_level >= int(threat_max):
+                            # Undo previous firewall rule and block firewall-cmd command
+                            threat_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            threat_notification = f'[{threat_time}] system at MAX THREAT LEVEL ({threat_max}), possible INCIDENT'
+
+                            print(threat_notification)
+                            socket.sendall(threat_notification.encode('utf-8'))
+
+                            block_watched_cmds(executed_watched_cmds, syscall_attrs['UID'], block_time, threat_file, socket)
+                            executed_watched_cmds = []
+                        elif current_threat_level >= int(threat_mid):
+                            # Only send alert of event if mid level threat
+                            threat_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            threat_notification = f'[{threat_time}] system at MEDIUM THREAT LEVEL ({threat_mid})'
+
+                            print(threat_notification)
+                            socket.sendall(threat_notification.encode('utf-8'))                    
